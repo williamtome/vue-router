6fe8dd7115340b6a3c3a01fe06771a794da0fdba
William Tomé <william.weirich@helioprint.com.br>
2023-12-11 11:24:10 -0300

    (feat): created Team route and page

---
+++ src/components/Team.vue
@@ -0,0 +1,29 @@
+<template>
+  <h1>Equipe</h1>
+
+  <p v-if="member === 'william'">
+    Bio William
+  </p>
+  <p v-else-if="member === 'jonsnow'">
+    Bio Jon Snow
+  </p>
+  <p v-else>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+    Beatae blanditiis commodi consequuntur dolorum eligendi enim,
+    exercitationem expedita ipsa itaque laboriosam laborum,
+    minus neque nisi odit quae quas sunt velit vero!
+  </p>
+
+  <button @click="$router.back()">Voltar</button>
+</template>
+
+<script>
+export default {
+  name: 'TeamPage',
+  computed: {
+    member() {
+      return this.$route.params.member
+    }
+  },
+}
+</script>
