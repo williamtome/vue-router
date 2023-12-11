50737f34470d298c62e4718109f22ab547835673
William Tomé <william.weirich@helioprint.com.br>
2023-12-11 11:23:39 -0300

    (feat): created Post page with link redirect to back on history navigation

---
+++ src/components/Post.vue
@@ -0,0 +1,29 @@
+<template>
+  <h1>Post</h1>
+
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+    Assumenda at blanditiis commodi corporis dolor dolore dolores
+    earum laborum nisi odit pariatur quaerat quas quidem quod,
+    repellendus saepe tempore vitae! Aliquid.
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+    Assumenda at blanditiis commodi corporis dolor dolore dolores
+    earum laborum nisi odit pariatur quaerat quas quidem quod,
+    repellendus saepe tempore vitae! Aliquid.</p>
+
+  <button @click.stop.prevent="goBack()">Voltar</button>
+</template>
+
+<script>
+export default {
+  name: "PostPage",
+  methods: {
+    goBack() {
+      this.$router.back()
+    }
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
